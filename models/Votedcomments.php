66a2913e2827a5a5813846f12fd7e49d85a442ef
Andrei Dumitrescu <andrei_dumitrescu2000@yahoo.ro>
2017-12-09 14:23:16 +0200

    Issues

---
+++ models/Votedcomments.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "votedcomments".
+ *
+ * @property integer $int
+ * @property string $member_id
+ * @property integer $comm_id
+ *
+ * @property Comments $comm
+ * @property Member $member
+ */
+class Votedcomments extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'votedcomments';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['member_id', 'comm_id'], 'integer'],
+            [['comm_id'], 'exist', 'skipOnError' => true, 'targetClass' => Comments::className(), 'targetAttribute' => ['comm_id' => 'id']],
+            [['member_id'], 'exist', 'skipOnError' => true, 'targetClass' => Member::className(), 'targetAttribute' => ['member_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'int' => 'Int',
+            'member_id' => 'Member ID',
+            'comm_id' => 'Comm ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComm()
+    {
+        return $this->hasOne(Comments::className(), ['id' => 'comm_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMember()
+    {
+        return $this->hasOne(Member::className(), ['id' => 'member_id']);
+    }
+}
---
+++ views/comments/plus.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Vote';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-login">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <h2> Thank you for your vote! </h2>
+
+    
+
+</div>
